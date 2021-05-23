20b8f87524a66107bcafb6fd6e16494152ad944e
Dhruv Maheshwari <mrdm026@gmail.com>
2021-05-23 08:12:22 +0530

    first commit

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.cyan[800],
+      ),
+    );
+  }
+}
