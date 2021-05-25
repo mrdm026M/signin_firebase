30e3cce66a19ee41952ba34b0f95f109e60a81f3
Dhruv Maheshwari <mrdm026@gmail.com>
2021-05-25 10:31:06 +0530

    created screen 2 and 3

---
+++ lib/screens/SignInScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:signin_firebase/styles/Colors.dart';
+
+class SignInScreen extends StatefulWidget {
+  SignInScreen({Key key}) : super(key: key);
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: AppColors.ColorBlack,
+    );
+  }
+}
---
+++ lib/screens/SignUpScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:signin_firebase/styles/Colors.dart';
+
+class SignUpScreen extends StatefulWidget {
+  SignUpScreen({Key key}) : super(key: key);
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: AppColors.ColorBlack,
+    );
+  }
+}
