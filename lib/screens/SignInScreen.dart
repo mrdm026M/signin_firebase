7e62332fc5cc49101857d832b5b7c3a2884dab25
Dhruv Maheshwari <mrdm026@gmail.com>
2021-05-25 16:01:05 +0530

    sign in ui complete

--- lib/screens/SignInScreen.dart
+++ lib/screens/SignInScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:signin_firebase/screens/HomeScreen.dart';
 import 'package:signin_firebase/styles/Colors.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -9,12 +10,228 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final _key = GlobalKey<FormState>();
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      color: AppColors.ColorBlack,
+    Size deviceSize = MediaQuery.of(context).size;
+
+    final ButtonStyle signUpButtonStyle = TextButton.styleFrom(
+      backgroundColor: AppColors.ColorWhite,
+      padding: EdgeInsets.symmetric(vertical: 15.0),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12.0)),
+      ),
+    );
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        width: deviceSize.width,
+        height: deviceSize.height,
+        color: AppColors.ColorBlack,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(
+                  top: 50.0, bottom: 45.0, left: 25.0, right: 25.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeScreen(),
+                        ),
+                      );
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 30.0,
+                      color: AppColors.ColorBlack,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Welcome \nBack!',
+                          style: TextStyle(
+                            fontSize: 32.5,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.ColorBlack,
+                            height: 1,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          'Continue your adventure.',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.ColorBlack,
+                            height: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: AppColors.ColorWhite,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50.0),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(
+                    top: 80.0, bottom: 60.0, left: 30.0, right: 30.0),
+                child: Column(
+                  children: [
+                    Form(
+                      key: _key,
+                      child: Column(
+                        children: [
+                          // email
+                          TextFormField(
+                            controller: _emailController,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Email cannot be empty';
+                              } else {
+                                return null;
+                              }
+                            },
+                            decoration: InputDecoration(
+                              isDense: true,
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: AppColors.ColorWhite),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: AppColors.ColorWhite),
+                              ),
+                              labelText: 'Email',
+                              labelStyle: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 12.5,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.ColorWhite.withOpacity(0.4),
+                                letterSpacing: 0.5,
+                              ),
+                            ),
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 17.5,
+                              color: AppColors.ColorWhite,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                          SizedBox(height: 12.5),
+                          // password
+                          TextFormField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Password cannot be empty';
+                              } else {
+                                return null;
+                              }
+                            },
+                            decoration: InputDecoration(
+                              isDense: true,
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: AppColors.ColorWhite),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: AppColors.ColorWhite),
+                              ),
+                              labelText: 'Password',
+                              labelStyle: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 12.5,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.ColorWhite.withOpacity(0.4),
+                                letterSpacing: 0.5,
+                              ),
+                            ),
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 17.5,
+                              color: AppColors.ColorWhite,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    Container(
+                      width: double.infinity,
+                      child: TextButton(
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.ColorBlack,
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                        style: signUpButtonStyle,
+                        onPressed: () {
+                          if (_key.currentState.validate()) {}
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 25.0),
+                    Container(
+                      width: double.infinity,
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 0.25,
+                          color: AppColors.ColorWhite,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
